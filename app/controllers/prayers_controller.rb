e4764e44738e0fe948fd865f41377b2f2196ee2d
Aaron Mills <aaron@jailbreakcreative.com>
2010-12-14 23:52:45 -0500

    Cleaned up the prayers controller

--- app/controllers/prayers_controller.rb
+++ app/controllers/prayers_controller.rb
@@ -1,8 +1,7 @@
 class PrayersController < ApplicationController
-  # GET /prayers
-  # GET /prayers.xml
+
   def index
-    @prayers = Prayer.all
+    @prayers = Prayer.order("prayers.position ASC")
 
     respond_to do |format|
       format.html # index.html.erb
@@ -10,74 +9,51 @@ class PrayersController < ApplicationController
     end
   end
 
-  # GET /prayers/1
-  # GET /prayers/1.xml
   def show
     @prayer = Prayer.find(params[:id])
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @prayer }
-    end
+  
   end
 
-  # GET /prayers/new
-  # GET /prayers/new.xml
   def new
     @prayer = Prayer.new
 
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @prayer }
-    end
   end
 
-  # GET /prayers/1/edit
   def edit
     @prayer = Prayer.find(params[:id])
   end
 
-  # POST /prayers
-  # POST /prayers.xml
+
   def create
     @prayer = Prayer.new(params[:prayer])
 
-    respond_to do |format|
       if @prayer.save
-        format.html { redirect_to(@prayer, :notice => 'Prayer was successfully created.') }
-        format.xml  { render :xml => @prayer, :status => :created, :location => @prayer }
+        redirect_to(@prayer, :notice => 'Thank you for submitting a prayer request.')
       else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @prayer.errors, :status => :unprocessable_entity }
+        render :action => "new" }
       end
-    end
+
   end
 
-  # PUT /prayers/1
-  # PUT /prayers/1.xml
+
   def update
     @prayer = Prayer.find(params[:id])
 
-    respond_to do |format|
       if @prayer.update_attributes(params[:prayer])
-        format.html { redirect_to(@prayer, :notice => 'Prayer was successfully updated.') }
-        format.xml  { head :ok }
+       redirect_to(@prayer, :notice => 'Prayer was successfully updated.')
       else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @prayer.errors, :status => :unprocessable_entity }
+       render :action => "edit"
       end
-    end
+
   end
 
-  # DELETE /prayers/1
-  # DELETE /prayers/1.xml
+
   def destroy
     @prayer = Prayer.find(params[:id])
     @prayer.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(prayers_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(prayers_url, :notice => "Your prayer request has been removed.")
+
   end
 end
