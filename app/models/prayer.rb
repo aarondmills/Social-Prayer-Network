c36ac83a477983a65160574eed328a044cb7221e
Aaron Mills <aaron@jailbreakcreative.com>
2010-12-16 21:57:16 -0500

    Improved validations

--- app/models/prayer.rb
+++ app/models/prayer.rb
@@ -1,4 +1,12 @@
 class Prayer < ActiveRecord::Base
+	attr_accessible :request, :category_id
+
 	belongs_to :category
 	belongs_to :user
+
+	validates_presence_of :request, :category_id
+
+	
+
+	
 end
