2f7683602e99eb168ac567e984b1abd14fa76128
Aaron Mills <aaron@jailbreakcreative.com>
2010-12-22 18:34:10 -0500

    Changed migrations

--- db/migrate/20101217070201_add_role_to_users.rb
+++ db/migrate/20101217070201_add_role_to_users.rb
@@ -1,9 +1,17 @@
 class AddRoleToUsers < ActiveRecord::Migration
   def self.up
     add_column :users, :role, :string
+    add_column :users, :firstname, :string
+    add_column :users, :lastname, :string
+    add_column :users, :location, :string
+    add_column :users, :avatar, :boolean
   end
 
   def self.down
     remove_column :users, :role
+    remove_column :users, :avatar
+    remove_column :users, :location
+    remove_column :users, :lastname
+    remove_column :users, :firstname
   end
 end
