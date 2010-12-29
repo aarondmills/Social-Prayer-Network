305a9dd67c7017520275284d99cf9b56b3ab4f7d
Aaron Mills <aaron@jailbreakcreative.com>
2010-12-29 17:10:38 -0500

    Changed cookie secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Spn::Application.config.secret_token = 'c21fa19756bc41e306c864d446f4265472693085358085c7886787671832cacdf13e7d8f4a1904823187c852fb5709718117ba3786b50521406e69c163d3bdf5'
+Spn::Application.config.secret_token = 'c21fa19756bc41e306c864d446f4295472693085358085c7886787671832cacdf13e7d8f4a1904823187c852fb5709718117ba3786b50521406e69c163d3bdf5'
